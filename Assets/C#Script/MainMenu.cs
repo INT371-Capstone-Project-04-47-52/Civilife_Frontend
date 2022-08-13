930a42304d468939dcb1c2c019ae6aab4ef2a39f
Kpinnoes <khorapin.pingadpu@mail.kmutt.ac.th>
2022-08-14 02:48:24 +0700

    [Update] Main Page

--- Assets/C#Script/MainMenu.cs
+++ Assets/C#Script/MainMenu.cs
@@ -5,9 +5,9 @@ using UnityEngine.SceneManagement;
 public class MainMenu : MonoBehaviour
 {
    public void PlayGame(){
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 2);
     }
      public void CreateAccount(){
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 2);
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
 }
